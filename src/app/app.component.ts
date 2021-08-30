57a631d6e2f7516e8baec2d441c574a5cd3732cc
falkowa42 <falkowa42@gmail.com>
2021-08-30 07:03:13 -0400

    Added in angular animations code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,100 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger,} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px)',
+        })
+      ),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          backgroundColor: 'orange',
+          transform: 'translateX(0px) scale(0.5)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'green',
+        }),
+        animate(
+          1000,
+          style({
+            borderRadius: '50px',
+          })
+        ),
+        animate(500),
+      ]),
+    ]),
+    trigger('list1', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [animate(300)]),
+    ]),
+  ],
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
+  wildState = 'normal';
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item: string) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAnimate() {
+    this.state === 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+    this.wildState === 'normal'
+      ? (this.wildState = 'highlighted')
+      : (this.wildState = 'normal');
+  }
 }
